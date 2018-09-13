8e10f1a9d86a05031298082f488463c4037bf831
Scoti Dodson <scotidodson@gmail.com>
2018-09-13 20:59:52 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,6 +3,5 @@
 def alphabetize(arr)
   esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   ascii = "@-\\"
-  arr.sort!
-  arr.sort_by{ |string| string[1].tr(esp_alph, ascii)}
+  arr.sort!.sort_by{ |string| string[1].tr(esp_alph, ascii)}
 end
