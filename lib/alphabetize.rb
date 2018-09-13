7c9199048bd65944be7dca97e6a926a8273a4cbb
Scoti Dodson <scotidodson@gmail.com>
2018-09-13 21:15:01 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,5 @@
-
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  ascii = "@-\\"
-  arr.sort_by{ |string| string[1].tr(esp_alph, ascii)}
+  arr..sort_by { |word| word.length}
 end
